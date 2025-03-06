0a222f85ed58af607f5aafdac8ec833382e44367
MD <manardg09@gmail.com>
2025-03-06 10:46:14 +0100

    app5.java

--- tp8(chaine de caractère)/app5.java
+++ tp8(chaine de caractère)/app5.java
@@ -7,7 +7,7 @@ public class app5 {
         System.out.print("Entrez une chaîne de caractères : ");
         String ch = scanner.nextLine();
 
-        StringBuilder result = new StringBuilder();
+        StringBuffer result = new StringBuffer();
         for (int i = 0; i < ch.length(); i++) {
             result.append(ch.charAt(i));
             if (i != ch.length() - 1) {
